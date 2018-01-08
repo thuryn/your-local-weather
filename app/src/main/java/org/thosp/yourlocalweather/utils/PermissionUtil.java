e059b59b5b52b4395f320d93329aeefe6f342efb
thuryn <Work4finance>
2018-01-08 22:04:22 +0100

    Request permissions on start of the app

--- app/src/main/java/org/thosp/yourlocalweather/utils/PermissionUtil.java
+++ app/src/main/java/org/thosp/yourlocalweather/utils/PermissionUtil.java
@@ -7,6 +7,10 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PermissionUtil {
 
     public static boolean verifyPermissions(int[] grantResults) {
@@ -23,15 +27,18 @@ public class PermissionUtil {
     }
 
     public static void checkAllPermissions(Context context) {
+        List<String> permissions = new ArrayList<>();
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions((Activity) context,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    123);
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.READ_PHONE_STATE);
+        }
+
+        if (!permissions.isEmpty()) {
             ActivityCompat.requestPermissions((Activity) context,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},
-                    124);
+                    permissions.toArray(new String[permissions.size()]),
+                    123);
         }
     }
 }
