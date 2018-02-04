2ff4e450a4710472e58eb53aad1941249e215d95
thuryn <Work4finance>
2018-02-04 18:14:52 +0100

    protect capitalization of texts for zero length strings

--- app/src/main/java/org/thosp/yourlocalweather/utils/Utils.java
+++ app/src/main/java/org/thosp/yourlocalweather/utils/Utils.java
@@ -466,6 +466,9 @@ public class Utils {
     }
 
     private static String capitalizeFirstLetter(String input) {
+        if ((input == null) || (input.length() < 1)) {
+            return "";
+        }
         return input.substring(0, 1).toUpperCase() + input.substring(1);
     }
 
