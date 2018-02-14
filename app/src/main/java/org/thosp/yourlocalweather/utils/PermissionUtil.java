331a543587076e3b64a86807fe9ea639b06d2708
tomas.huryn@apollogames.com <Work4finance>
2018-02-14 22:43:01 +0100

    System Location permission requested with Local MLS set #20 - fix permission checking, added logging in permission check procedure

--- app/src/main/java/org/thosp/yourlocalweather/utils/PermissionUtil.java
+++ app/src/main/java/org/thosp/yourlocalweather/utils/PermissionUtil.java
@@ -1,24 +1,21 @@
 package org.thosp.yourlocalweather.utils;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
-import org.thosp.yourlocalweather.MainActivity;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.thosp.yourlocalweather.utils.LogToFile.appendLog;
+import static org.thosp.yourlocalweather.utils.LogToFile.appendLogWithParams;
 
 public class PermissionUtil {
+
+    private static final String TAG_CHECK_PERMISSIONS_AND_SETTINGS = "PermissionUtil:checkPermissionsAndSettings";
 
     public static boolean verifyPermissions(int[] grantResults) {
         if (grantResults.length < 1) {
@@ -69,5 +66,49 @@ public class PermissionUtil {
             }
         }
         return permissions;
+    }
+
+    public static boolean checkPermissionsAndSettings(Context context) {
+        String locationUpdateStrategy = PreferenceManager.getDefaultSharedPreferences(context).getString(
+                Constants.KEY_PREF_LOCATION_UPDATE_STRATEGY, "update_location_full");
+        if ("update_location_none".equals(locationUpdateStrategy)) {
+            appendLog(context, TAG_CHECK_PERMISSIONS_AND_SETTINGS, "locationUpdateStrategy is set to update_location_none, return false");
+            return false;
+        }
+
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        boolean isGPSEnabled = locationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER)
+                && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = locationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER)
+                && locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        appendLog(context, TAG_CHECK_PERMISSIONS_AND_SETTINGS, "isGPSEnabled=" + isGPSEnabled + ", isNetworkEnabled=" + isNetworkEnabled);
+        if (!isGPSEnabled && !isNetworkEnabled) {
+            appendLog(context, TAG_CHECK_PERMISSIONS_AND_SETTINGS, "isGPSEnabled and isNetworkEnabled is not set, returning false");
+            return false;
+        } else {
+            String geocoder = AppPreference.getLocationGeocoderSource(context);
+            List<String> permissions = new ArrayList<>();
+            if (AppPreference.isGpsEnabledByPreferences(context) &&
+                    isGPSEnabled &&
+                    ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (isNetworkEnabled) {
+                if ("location_geocoder_local".equals(geocoder) && ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    permissions.add(Manifest.permission.READ_PHONE_STATE);
+                } else if ("location_geocoder_system".equals(geocoder) && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+                }
+            }
+            appendLog(context, TAG_CHECK_PERMISSIONS_AND_SETTINGS, "permissions is empty = " + permissions.isEmpty());
+            if (permissions.isEmpty()) {
+                appendLog(context, TAG_CHECK_PERMISSIONS_AND_SETTINGS, "permissions is empty, returning true");
+                return true;
+            } else {
+                appendLogWithParams(context, TAG_CHECK_PERMISSIONS_AND_SETTINGS, "permissions is not empty, returning false, permissions = ", permissions);
+                return false;
+            }
+        }
     }
 }
