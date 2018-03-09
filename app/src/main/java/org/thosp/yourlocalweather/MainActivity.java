09188799e693fa95e4cae644b3e26badf0eeaf47
Tomas <thuryn1@gmail.com>
2018-03-09 23:19:05 +0100

    #12, #23 - hide location search button when auto location disabled

--- app/src/main/java/org/thosp/yourlocalweather/MainActivity.java
+++ app/src/main/java/org/thosp/yourlocalweather/MainActivity.java
@@ -180,6 +180,10 @@ public class MainActivity extends BaseActivity implements AppBarLayout.OnOffsetC
         this.mToolbarMenu = menu;
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.activity_main_menu, menu);
+        Location autoLocation = locationsDbHelper.getLocationByOrderId(0);
+        if (!autoLocation.isEnabled()) {
+            menu.findItem(R.id.main_menu_detect_location).setVisible(false);
+        }
         return true;
     }
 
