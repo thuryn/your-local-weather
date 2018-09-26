ee147f97daae39d7828c7bf3121485b8743c6ee5
Tomas <thuryn1@gmail.com>
2018-09-26 22:03:39 +0200

    Add check for null values in location and address extras

--- app/src/main/java/org/thosp/yourlocalweather/service/LocationUpdateService.java
+++ app/src/main/java/org/thosp/yourlocalweather/service/LocationUpdateService.java
@@ -248,8 +248,14 @@ public class LocationUpdateService extends AbstractCommonService implements Loca
         if (intent.getExtras() == null) {
             return;
         }
-        Location inputLocation = (Location) intent.getExtras().getParcelable("inputLocation");
-        Address addresses = (Address) intent.getExtras().getParcelable("addresses");
+        Location inputLocation = null;
+        if (intent.getExtras().getParcelable("inputLocation") != null) {
+            inputLocation = (Location) intent.getExtras().getParcelable("inputLocation");
+        }
+        Address addresses = null;
+        if (intent.getExtras().getParcelable("addresses") != null) {
+            addresses = (Address) intent.getExtras().getParcelable("addresses");
+        }
         appendLog(getBaseContext(), TAG, "LOCATION_UPDATE recieved:" + inputLocation + ":" + addresses);
         onLocationChanged(inputLocation, addresses);
     }
