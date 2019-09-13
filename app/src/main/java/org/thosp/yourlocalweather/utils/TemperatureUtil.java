162a5b11e29e34cceea4cda5d19310dc63aeaf33
Tomáš Hurýn <thuryn1@gmail.com>
2019-09-13 19:50:02 +0200

    #138 - fix calculation of the dew point

--- app/src/main/java/org/thosp/yourlocalweather/utils/TemperatureUtil.java
+++ app/src/main/java/org/thosp/yourlocalweather/utils/TemperatureUtil.java
@@ -159,18 +159,16 @@ public class TemperatureUtil {
         }
         String unitsFromPreferences = PreferenceManager.getDefaultSharedPreferences(context).getString(
                 Constants.KEY_PREF_TEMPERATURE_UNITS, "celsius");
-        double humidityLogarithm = Math.log(weather.getHumidity() / 100) / Math.log(Math.E);
+        double humidityLogarithm = Math.log(weather.getHumidity() / 100.0) / Math.log(Math.E);
         double dewPointPart = humidityLogarithm + ((17.67 * weather.getTemperature())/(243.5 + weather.getTemperature()));
         double dewPoint = (243.5 * dewPointPart) / (17.67 - dewPointPart);
-        appendLog(context, "TemperatureUtil", "humidityLogarithm=" + humidityLogarithm + ", dewPointPart=" + dewPointPart +
-                ", dewPoint=" + dewPoint);
         if (unitsFromPreferences.contains("fahrenheit") ) {
             double fahrenheitValue = (dewPoint * 1.8f) + 32;
-            return String.format(locale, "%d",
-                    Math.round(fahrenheitValue)) + getTemperatureUnit(context);
+            return String.format(locale, "%.1f",
+                    fahrenheitValue) + getTemperatureUnit(context);
         } else {
-            return String.format(locale, "%d",
-                    Math.round(dewPoint)) + getTemperatureUnit(context);
+            return String.format(locale, "%.1f",
+                    dewPoint) + getTemperatureUnit(context);
         }
     }
     
