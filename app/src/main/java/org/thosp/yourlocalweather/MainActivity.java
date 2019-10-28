c212b185bbd93df77eaf36c78afc8976d732c8d0
Tomáš Hurýn <thuryn1@gmail.com>
2019-10-28 20:41:40 +0100

    Added translation statement into popup and help

--- app/src/main/java/org/thosp/yourlocalweather/MainActivity.java
+++ app/src/main/java/org/thosp/yourlocalweather/MainActivity.java
@@ -727,6 +727,28 @@ public class MainActivity extends BaseActivity
         return false;
     }
 
+    private void showVoiceAndSourcesDisclaimer() {
+        int initialGuideVersion = PreferenceManager.getDefaultSharedPreferences(getBaseContext())
+                .getInt(Constants.APP_INITIAL_GUIDE_VERSION, 0);
+        if (initialGuideVersion != 3) {
+            return;
+        }
+        final Context localContext = getBaseContext();
+        final AlertDialog.Builder settingsAlert = new AlertDialog.Builder(MainActivity.this);
+        settingsAlert.setTitle(R.string.alertDialog_voice_disclaimer_title);
+        settingsAlert.setMessage(R.string.alertDialog_voice_disclaimer_message);
+        settingsAlert.setNeutralButton(R.string.alertDialog_battery_optimization_proceed,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences.Editor preferences = PreferenceManager.getDefaultSharedPreferences(localContext).edit();
+                        preferences.putInt(Constants.APP_INITIAL_GUIDE_VERSION, 4);
+                        preferences.apply();
+                    }
+                });
+        settingsAlert.show();
+    }
+
     private void checkBatteryOptimization() {
         int initialGuideVersion = PreferenceManager.getDefaultSharedPreferences(getBaseContext())
                 .getInt(Constants.APP_INITIAL_GUIDE_VERSION, 0);
@@ -793,6 +815,7 @@ public class MainActivity extends BaseActivity
             if (initialGuideVersion < 3) {
                 checkBatteryOptimization();
             }
+            showVoiceAndSourcesDisclaimer();
             return;
         }
         if (initialGuidePage > 0) {
