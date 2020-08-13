972dca19fd39ec4f4782d0a0ad0f2e4913599d73
iillyyaa2033 <iillyyaa2033@mail.ru>
2020-08-13 17:52:41 +0300

    Highlight highest and lowest (closes #76)

--- app/src/main/java/org/thosp/yourlocalweather/adapter/WeatherForecastItemViewHolder.java
+++ app/src/main/java/org/thosp/yourlocalweather/adapter/WeatherForecastItemViewHolder.java
@@ -1,11 +1,9 @@
 package org.thosp.yourlocalweather.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.format.DateFormat;
-import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -17,15 +15,11 @@ import org.thosp.yourlocalweather.utils.AppPreference;
 import org.thosp.yourlocalweather.utils.TemperatureUtil;
 import org.thosp.yourlocalweather.utils.Utils;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
-public class WeatherForecastItemViewHolder  extends RecyclerView.ViewHolder {
+public class WeatherForecastItemViewHolder extends RecyclerView.ViewHolder {
 
     private final String TAG = "ForecastViewHolder";
 
@@ -60,15 +54,10 @@ public class WeatherForecastItemViewHolder  extends RecyclerView.ViewHolder {
         mDescription = (TextView) itemView.findViewById(R.id.forecast_description);
     }
 
-    void bindWeather(Context context,
-                     double latitude,
-                     Locale locale,
-                     DetailedWeatherForecast weather,
-                     Set<Integer> visibleColumns) {
+    void bindWeather(Context context, double latitude, Locale locale, DetailedWeatherForecast weather, Set<Integer> visibleColumns, boolean isMin, boolean isMax) {
         mWeatherForecast = weather;
 
-        Typeface typeface = Typeface.createFromAsset(mContext.getAssets(),
-                "fonts/weathericons-regular-webfont.ttf");
+        Typeface typeface = Typeface.createFromAsset(mContext.getAssets(), "fonts/weathericons-regular-webfont.ttf");
         WeatherCondition weatherCondition = weather.getFirstWeatherCondition();
 
         if (visibleColumns.contains(1)) {
@@ -76,7 +65,7 @@ public class WeatherForecastItemViewHolder  extends RecyclerView.ViewHolder {
             Date date = new Date(weather.getDateTime() * 1000);
             mTime.setText(AppPreference.getLocalizedTime(context, date, locale));
             if (AppPreference.is12TimeStyle(context)) {
-                ViewGroup.LayoutParams params=mTime.getLayoutParams();
+                ViewGroup.LayoutParams params = mTime.getLayoutParams();
                 params.width = Utils.spToPx(85, context);
                 mTime.setLayoutParams(params);
             }
@@ -102,16 +91,21 @@ public class WeatherForecastItemViewHolder  extends RecyclerView.ViewHolder {
         }
         if (visibleColumns.contains(4)) {
             mTemperature.setVisibility(View.VISIBLE);
-            String temperature = mContext.getString(R.string.temperature_with_degree,
-                    TemperatureUtil.getForecastedTemperatureWithUnit(mContext, weather, locale));
+            String temperature = mContext.getString(R.string.temperature_with_degree, TemperatureUtil.getForecastedTemperatureWithUnit(mContext, weather, locale));
             mTemperature.setText(temperature);
+            if (isMin == isMax) {
+                mTemperature.setTextColor(Color.BLACK);
+            } else if (isMax) {
+                mTemperature.setTextColor(Color.RED);
+            } else {
+                mTemperature.setTextColor(Color.BLUE);
+            }
         } else {
             mTemperature.setVisibility(View.GONE);
         }
         if (visibleColumns.contains(5)) {
             mApparentTemperature.setVisibility(View.VISIBLE);
-            String apparentTemperature = mContext.getString(R.string.temperature_with_degree,
-                    TemperatureUtil.getForecastedApparentTemperatureWithUnit(mContext, latitude, weather, locale));
+            String apparentTemperature = mContext.getString(R.string.temperature_with_degree, TemperatureUtil.getForecastedApparentTemperatureWithUnit(mContext, latitude, weather, locale));
             mApparentTemperature.setText(apparentTemperature);
         } else {
             mApparentTemperature.setVisibility(View.GONE);
@@ -145,7 +139,7 @@ public class WeatherForecastItemViewHolder  extends RecyclerView.ViewHolder {
                     mRainSnow.setText(snow);
                 }
             }
-            ViewGroup.LayoutParams params=mRainSnow.getLayoutParams();
+            ViewGroup.LayoutParams params = mRainSnow.getLayoutParams();
             params.width = Utils.spToPx(AppPreference.getRainOrSnowForecastWeadherWidth(context), context);
             mRainSnow.setLayoutParams(params);
         } else {
