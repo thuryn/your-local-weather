36d90b5b1592d52d60f72c8557c3eeae2f3a574b
Tomáš Hurýn <huryn@2n.com>
2026-02-24 21:05:11 +0100

    Updated versions

--- app/src/main/java/org/thosp/yourlocalweather/AddVoiceSettingActivity.java
+++ app/src/main/java/org/thosp/yourlocalweather/AddVoiceSettingActivity.java
@@ -67,8 +67,8 @@ public class AddVoiceSettingActivity extends BaseActivity {
             timeStylePreference = AppPreference.getTimeStylePreference(this);
 
             updateItemsFromDb();
+            populateTriggerType();
         });
-        populateTriggerType();
     }
 
     @Override
@@ -511,52 +511,56 @@ public class AddVoiceSettingActivity extends BaseActivity {
     }
 
     public void populateTriggerType() {
-        Spinner spinner = findViewById(R.id.trigger_type);
         boolean btNotPresentOrEnabled = checkExistenceAndBtPermissions();
 
-        ArrayAdapter<CharSequence> adapter;
-        if (!btNotPresentOrEnabled) {
-            adapter = ArrayAdapter.createFromResource(this,
-                    R.array.voice_trigger_type_wo_bt, android.R.layout.simple_spinner_item);
-        } else {
-            adapter = ArrayAdapter.createFromResource(this,
-                    R.array.voice_trigger_type, android.R.layout.simple_spinner_item);
-        }
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
         Long currentTriggerId = voiceSettingParametersDbHelper.getLongParam(
                 voiceSettingId,
                 VoiceSettingParamType.VOICE_SETTING_TRIGGER_TYPE.getVoiceSettingParamTypeId());
-        if (currentTriggerId != null) {
-            int currentTriggerIdInt = currentTriggerId.intValue();
-            if (!btNotPresentOrEnabled && (currentTriggerIdInt == 2)) {
-                currentTriggerIdInt = 1;
+
+        runOnUiThread(() -> {
+            Spinner spinner = findViewById(R.id.trigger_type);
+            ArrayAdapter<CharSequence> adapter;
+            if (!btNotPresentOrEnabled) {
+                adapter = ArrayAdapter.createFromResource(this,
+                        R.array.voice_trigger_type_wo_bt, android.R.layout.simple_spinner_item);
+            } else {
+                adapter = ArrayAdapter.createFromResource(this,
+                        R.array.voice_trigger_type, android.R.layout.simple_spinner_item);
             }
-            spinner.setSelection(currentTriggerIdInt);
-            triggerTypeChanged(currentTriggerIdInt);
-        }
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                int positionToSave = position;
-                if (!btNotPresentOrEnabled && (position == 1)) {
-                    positionToSave++;
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+
+            if (currentTriggerId != null) {
+                int currentTriggerIdInt = currentTriggerId.intValue();
+                if (!btNotPresentOrEnabled && (currentTriggerIdInt == 2)) {
+                    currentTriggerIdInt = 1;
                 }
-                voiceSettingParametersDbHelper.saveLongParam(
-                        voiceSettingId,
-                        VoiceSettingParamType.VOICE_SETTING_TRIGGER_TYPE.getVoiceSettingParamTypeId(),
-                        positionToSave);
+                spinner.setSelection(currentTriggerIdInt);
+                triggerTypeChanged(currentTriggerIdInt);
+            }
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    int positionToSave = position;
+                    if (!btNotPresentOrEnabled && (position == 1)) {
+                        positionToSave++;
+                    }
+                    voiceSettingParametersDbHelper.saveLongParam(
+                            voiceSettingId,
+                            VoiceSettingParamType.VOICE_SETTING_TRIGGER_TYPE.getVoiceSettingParamTypeId(),
+                            positionToSave);
 
-                triggerTypeChanged(positionToSave);
-                if (positionToSave == 2) {
-                    prepareNextTime(AddVoiceSettingActivity.this, voiceSettingId, timeStylePreference, applicationLocale, voiceSettingParametersDbHelper);
+                    triggerTypeChanged(positionToSave);
+                    if (positionToSave == 2) {
+                        prepareNextTime(AddVoiceSettingActivity.this, voiceSettingId, timeStylePreference, applicationLocale, voiceSettingParametersDbHelper);
+                    }
                 }
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
 
-            }
+                }
+            });
         });
     }
 
